f5b0322232cd973374a9b9347b4d1d49b337c965
unknown <jess7642@gmail.com>
2018-08-28 09:12:05 -0400

    beginning db coffee

---
+++ Lab23/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab23.Models;
+
+namespace Lab23.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CoffeShopDBEntities ORM = new CoffeShopDBEntities();
+            ViewBag.Items = ORM.Items.ToList();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AddUser()
+        {
+            CoffeShopDBEntities ORM = new CoffeShopDBEntities();
+            ViewBag.User = ORM.Users.ToList();
+            return View();
+
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+
+        }
+    }
+}
